deb295c51ae761b00d777bffbe867af72436c2a4
Angus chua <angusc8774@gmail.com>
2022-07-23 09:16:55 +0800

    Initial Commit

---
+++ Boink Boink Doink/Boink_Boink_DoinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boink_Boink_DoinkApp.swift
+//  Boink Boink Doink
+//
+//  Created by T Krobot on 23/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Boink_Boink_DoinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
